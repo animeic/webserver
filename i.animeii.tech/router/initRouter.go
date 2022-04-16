af21a70e395c36316b038fd29fb5bdd6cdccb295
animeii <animeic@163.com>
2022-04-16 14:41:20 +0800

    fix

--- i.animeii.tech/router/initRouter.go
+++ i.animeii.tech/router/initRouter.go
@@ -10,22 +10,6 @@ import (
 func InitRouter(app *gin.Engine) {
 	// 静态路由
 	app.Use(middleware.Cors())
-	app.StaticFile("/", "./static/i.animeii.tech/index.html")
-	app.StaticFile("/searchindex.xml", "./static/i.animeii.tech/searchindex.xml")
-	app.StaticFile("/sitemap.xml", "./static/i.animeii.tech/sitemap.xml")
-	app.Static("/img", "./static/i.animeii.tech/img")
-	app.StaticFS("/images", http.Dir("./static/i.animeii.tech/images"))
-	// app.Static("/images", "./static/i.animeii.tech/images")
-	app.StaticFile("/favicon.ico", "./static/i.animeii.tech/img/favicon.ico")
-	app.Static("/css", "./static/i.animeii.tech/css")
-	app.Static("/js", "./static/i.animeii.tech/js")
-	app.Static("/post", "./static/i.animeii.tech/post")
-	app.Static("/categories", "./static/i.animeii.tech/categories")
-	app.Static("/tags", "./static/i.animeii.tech/tags")
-	app.Static("/p", "./static/i.animeii.tech/p")
-	// anime page
-	app.Static("/anime", "./static/i.animeii.tech/anime")
-	app.Static("/about", "./static/i.animeii.tech/about")
-	// static images
-	app.Static("/_images", "./static/i.animeii.tech/_images")
+	app.StaticFS("/", http.Dir("./static/i.animeii.tech"))
+
 }
